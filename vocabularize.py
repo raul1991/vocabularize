601e47ed05ff8ae14dffd196bbe75fa1c3c96f9f
Rahul Bawa <rahul.bawa@ericsson.com>
2018-10-15 11:10:26 +0530

    Fix issues
    fixes #1
    fixes #3
    fixes #5

--- vocabularize.py
+++ vocabularize.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
-import subprocess # to help us execute os commands
-import time # to help us sleep between consecutive words
-from random import randrange # for random words
-list_of_words=[] # list of dict of words - name, synonyms and sentence
-def loadWords():
+import subprocess  # to help us execute os commands
+import time  # to help us sleep between consecutive words
+import random
+import sys  # parsing args
+
+list_of_words = []  # list of dict of words - name, synonyms and sentence
+
+
+def load_words():
+    del list_of_words[:]  # clearing the list
     words = open("words.txt", "r")
     for line in words.readlines():
-       name,synonyms,sentence = line.split("->")
-       word = {
-               'name': name, 
-               'synonyms': synonyms.split(","), 
-               'sentence': sentence
-              }
-       append_word(word)
+        name, synonyms, sentence = line.split("->")
+        word = {
+            'name': name.lower().strip(),
+            'synonyms': synonyms.split(","),
+            'sentence': sentence
+        }
+        append_word(word)
 
 
 def append_word(word):
@@ -20,10 +25,10 @@ def append_word(word):
 
 
 def quiz():
-    total_len = len(list_of_words)
-    while(True):
-        notify(list_of_words[randrange(total_len)])
-        time.sleep(20 * 60) # 20 minutes sleep
+    random.shuffle(list_of_words)
+    for idx in list_of_words:
+        notify(idx)
+        time.sleep(20 * 60)  # 20 minutes sleep
 
 
 def display(word):
@@ -33,8 +38,42 @@ def display(word):
 
 
 def notify(word):
-    subprocess.Popen(['notify-send', '-a' , 'vocabularize', '--urgency=critical' , word['name'].upper() +"\r\n", str(word['synonyms']) + "\r\n" + str(word['sentence'])])
+    subprocess.Popen(['notify-send', '-a', 'vocabularize', '--urgency=critical', word['name'].upper() + "\r\n",
+                      str(word['synonyms']) + "\r\n" + str(word['sentence'])])
 
 
-loadWords()
-quiz()
+def add_word():
+    name = raw_input('Enter a word : ')
+    if not word_exists(name):
+        synonyms = raw_input('Enter the synonyms (separated by a comma): ')
+        sentence = raw_input('Enter a sentence : ')
+        word_file.write("%s -> %s -> %s" % (name, synonyms, sentence))
+        return True
+    else:
+        return False
+
+def word_exists(name):
+    for w in list_of_words:
+        if w.get('name') == name:
+            return True
+
+
+def play_quiz():
+    while (True):
+        quiz()
+
+
+load_words()
+should_add_word = (sys.argv[1] == '--add_words')
+if should_add_word:
+    more = 'y'
+    word_file = open('words.txt', 'a+')
+    while more == 'y' or more == 'yes':
+        if not add_word():
+            print("Word already exists")
+            more = raw_input('Do you want to add more words (y/n) ?')
+
+should_play_quiz = str(raw_input('Want to play a quiz')).strip().lower()
+if should_play_quiz == 'y' or should_play_quiz == 'yes':
+    load_words()
+    play_quiz()
